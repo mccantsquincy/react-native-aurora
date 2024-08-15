9ba16d1ae5e8c4fad7ef810004f51b6eb3cdd17d
mccantsquincy <quincymccants@gmail.com>
2024-08-15 12:01:33 -0700

    setup nativewind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,16 @@
 import { StyleSheet, Text, View } from "react-native";
-import { Slot } from "expo-router";
+import { Slot, Stack } from "expo-router";
 
 const RootLayout = () => {
-  return <Slot />
-}
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }}/>
+      </Stack>
+    </>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
+
+
